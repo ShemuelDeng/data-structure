e2b4e0f8d6fa9d8ad791c4bbdd452dc0c1556266
shemuel <1241916209@qq.com>
2020-01-06 00:14:35 +0800

    add hanoitower

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -10,6 +10,13 @@ public class BinarySearch {
     private static int[] array = {12,13,14,21,23,34,55};
     private static int times = 0;
 
+    /**
+     * 递归方式
+     * @param left
+     * @param right
+     * @param key
+     * @return
+     */
     public static int binarySearch(int left,int right,int key){
 
         times++;
@@ -31,13 +38,45 @@ public class BinarySearch {
         }
     }
 
+    /**
+     * 递归方式
+     * @param key
+     * @return
+     */
     public static int binarySearch(int key){
        return  binarySearch(0,array.length-1,key);
     }
 
+
+    /**
+     * 非递归方式
+     * @param key
+     * @return
+     */
+    public static int binarySearchNormal(int key){
+        int left = 0;
+        int right = array.length-1;
+        while(left<right){
+            int mid = left+right/2;
+            if (array[mid] == key){
+                return mid;
+            }else if(array[mid]>key){
+                // 向左查找
+                right = right -1;
+            }else{
+                // 向右查找
+                left = left + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int index = binarySearch(12);
+        int index = binarySearch(33);
+        int index1 = binarySearchNormal(33);
         System.out.println(index);
+        System.out.println(index1);
+
         System.out.println(times);
     }
 
